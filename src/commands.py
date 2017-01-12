c3558b5382ab7b7963942fe104caabbc3825a767
Wesley Jones <joneswe@sheridancollege.ca>
2017-01-11 21:15:14 -0500

    Update commands.py
    Lobster wanted a period

--- src/commands.py
+++ src/commands.py
@@ -44,7 +44,7 @@ obey_dict = {
         'Mori': ':sheep: baaa',
         'infiniteimoc': 'I obey, Imoc, Herald of the Sun.',
         'neosloth': 'Long days and pleasant nights, neosloth.',
-        'Lobster': 'I obey, Spice Sommelier Lobster'
+        'Lobster': 'I obey, Spice Sommelier Lobster.'
 }
 
 # ---------------------------
